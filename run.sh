c708c94ed9b573a003d7c32570a7f6f0b51e7fb2
dmitry <dmitry_cec2@mail.ru>
2022-01-25 18:05:39 +0300

    commit 40

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ JMETER_VERSION=${JMETER_VERSION:-"5.4"}
 IMAGE="justb4/jmeter:${JMETER_VERSION}"
 
 # Finally run
-docker run --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
+docker run --rm --name ${NAME}$1 -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} $@
